40083091867de77d7fdf0e56a947d9184e9ff746
Heewon Ahn <57425238+shelost@users.noreply.github.com>
2024-10-02 21:06:47 -0400

    project navbar + styling

--- @/js/project.js
+++ @/js/project.js
@@ -1,4 +1,4 @@
-var elem = document.getElementById('gallery')
+var elem = document.getElementById('main')
 
 var msnry = new Masonry(elem, {
     // options
@@ -20,3 +20,23 @@ function Resize() {
 
 Resize()
 window.addEventListener('resize', Resize)
+
+
+let lastScrollTop = 0;
+const navbar = document.getElementById('navbar');
+const navbarHeight = navbar.offsetHeight;
+
+window.addEventListener('scroll', () => {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    // If scrolling down, move the navbar up
+    if (currentScroll > lastScrollTop) {
+        navbar.style.top = `-${navbarHeight}px`; // Move the navbar out of view
+    }
+    // If scrolling up, bring the navbar back down
+    else {
+        navbar.style.top = '15px'; // Reset navbar to its original position
+    }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+});
---
+++ @/js/script.js
@@ -0,0 +1,26 @@
+
+let lastScrollTop = 0;
+
+try {
+    const navbar = document.getElementById('navbar');
+
+    const navbarHeight = navbar.offsetHeight;
+
+    window.addEventListener('scroll', () => {
+        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        // If scrolling down, move the navbar up
+        if (currentScroll > lastScrollTop) {
+            navbar.style.top = `-${navbarHeight}px`; // Move the navbar out of view
+        }
+        // If scrolling up, bring the navbar back down
+        else {
+            navbar.style.top = '15px'; // Reset navbar to its original position
+        }
+
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+    });
+
+} catch {
+
+}
