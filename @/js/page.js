d76cd9571bfb66b494d8d83442942f262a92eb8a
Heewon Ahn <57425238+shelost@users.noreply.github.com>
2024-10-05 10:03:20 -0400

    Removed navbar on pages

--- @/js/page.js
+++ @/js/page.js
@@ -52,3 +52,33 @@ Resize()
 
 window.addEventListener('scroll', Scroll)
 window.addEventListener('resize', Resize)
+
+
+
+
+
+let lastScrollTop = 0;
+
+try {
+    const navbar = document.getElementById('navbar');
+
+    const navbarHeight = navbar.offsetHeight;
+
+    window.addEventListener('scroll', () => {
+        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        // If scrolling down, move the navbar up
+        if (currentScroll > lastScrollTop) {
+            navbar.style.top = `-${navbarHeight}px`; // Move the navbar out of view
+        }
+        // If scrolling up, bring the navbar back down
+        else {
+            navbar.style.top = '8px'; // Reset navbar to its original position
+        }
+
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+    });
+
+} catch {
+
+}
